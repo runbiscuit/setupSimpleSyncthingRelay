8023cca8f46b7362254c199bce11f5e23cf9a72b
sayem314 <sayem314@gmail.com>
2016-05-23 12:41:00 +0600

    Major update (32bit and arm supported now)
    Formatted better
    Added detection for architecture
    Fixed not downloading config files via bash

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo ""
 echo "Simple Syncthing Relay Setup Script by @theroyalstudent (Edwin A.)"
 echo "============ [ GitHub Source: https://git.io/vr2xt ] ============="
 echo "=================================================================="
@@ -12,8 +13,7 @@ rm -rf relaysrv* /etc/relaysrv /home/relaysrv /usr/local/bin/relaysrv > /dev/nul
 deluser relaysrv > /dev/null &> /dev/null
 
 echo ""
-echo "Please enter a relay name."
-read relayName
+read -p "Please enter a relay name: " relayName
 
 echo "You have entered '$relayName' as a relay name."
 
@@ -29,38 +29,63 @@ echo "Thus, on the Syncthing Relay page at relays.syncthing.net, it will show as
 echo "$relayName$delimiter$(wget ipinfo.io/city -qO -), $(wget ipinfo.io/country -qO -)"
 
 echo ""
-echo "Updating apt repositories..."
-apt-get update > /dev/null
+echo -n "Updating apt repositories..."
+apt-get update -y &>/dev/null
+echo "$(tput setaf 2)DONE$(tput sgr0)"
 
 echo ""
 echo "Installing packages: dtrx, sed, supervisor, wget if not installed yet."
-apt-get install dtrx sed supervisor wget -y > /dev/null
+apt-get install dtrx sed supervisor wget -y &>/dev/null
+
+#Detect architecture
+if [ -n "$(uname -m | grep 64)" ]; then
+	cpubits="linux-amd64"
+	cpubitsname="for (64bit)..."
+elif [ -n "$(uname -m | grep 86)" ]; then
+	cpubits="linux-386"
+	cpubitsname="for (32bit)..."
+elif [ -n "$(uname -m | grep armv*)" ]; then
+	cpubits="linux-arm"
+	cpubitsname="for (ARM)..."
+else
+	echo "unsupported or unknown architecture"
+	echo ""
+	exit;
+fi
 
 echo ""
-echo "Downloading latest release of the relaysrv daemon..."
-wget $(wget https://api.github.com/repos/syncthing/relaysrv/releases/latest -qO - | grep 'browser_' | grep 'linux-amd64' | cut -d\" -f4)
+echo "Downloading latest release of the relaysrv daemon $cpubitsname"
+cd
+wget $(wget https://api.github.com/repos/syncthing/relaysrv/releases/latest -qO - | grep 'browser_' | grep $cpubits | cut -d\" -f4) &>/dev/null
 
 echo ""
-echo "Extracting the relaysrv daemon..."
-dtrx relaysrv-linux-amd64.tar.gz
+echo -n "Extracting the relaysrv daemon..."
+dtrx relaysrv-linux*
+echo "	$(tput setaf 2)DONE$(tput sgr0)"
+
 
 echo ""
-echo "Moving the relaysrv daemon to /usr/local/bin..."
-mv relaysrv-linux-amd64/relaysrv /usr/local/bin
+echo -n "Moving the relaysrv daemon to /usr/local/bin..."
+cd relaysrv-linux*
+mv relaysrv /usr/local/bin
+echo "  $(tput setaf 2)DONE$(tput sgr0)"
 
 echo ""
-echo "Clearing up the remains of the relaysrv daemon."
-rm -rf relaysrv-linux-amd64*
+echo -n "Clearing up the remains of the relaysrv daemon."
+cd
+rm -rf relaysrv-linux*
+echo "  $(tput setaf 2)DONE$(tput sgr0)"
 
 echo ""
-echo "Adding a user for relaysrv, called relaysrv."
+echo -n "Adding a user for relaysrv, called relaysrv."
 adduser relaysrv --gecos '' --disabled-password
 mkdir /etc/relaysrv
 chown relaysrv /etc/relaysrv
+echo "  $(tput setaf 2)DONE$(tput sgr0)"
 
 echo ""
-echo "Copying Syncthing Relay supervisord configuration to the respective folder..."
-cp syncthingRelay.conf /etc/supervisor/conf.d/syncthingRelay.conf
+echo -n "Copying Syncthing Relay supervisord configuration to the respective folder..."
+wget -q "https://raw.githubusercontent.com/theroyalstudent/setupSimpleSyncthingRelay/master/syncthingRelay.conf" -O "/etc/supervisor/conf.d/syncthingRelay.conf"
 
 echo ""
 echo "Setting geolocation of the Syncthing relay."
@@ -70,6 +95,9 @@ echo ""
 echo "Restarting supervisord..."
 sudo service supervisor restart
 
-echo "And you should be up and running!"
+echo ""
+echo "And you should be up and running! (http://relays.syncthing.net)"
 echo "If this script worked, feel free to give my script a star!"
 echo "Exiting."
+echo ""
+exit 0
